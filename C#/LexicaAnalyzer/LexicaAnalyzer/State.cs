272ba8966ebbaef0e51e323096d581800981509b
Michael Fryer <michael.d.fryer@gmail.com>
2017-10-07 12:41:23 -0600

    Documentation for `State` and edited documentation for `Token`

--- C#/LexicaAnalyzer/LexicaAnalyzer/State.cs
+++ C#/LexicaAnalyzer/LexicaAnalyzer/State.cs
@@ -2,20 +2,76 @@
 using System.Collections.Generic;
 
 namespace SmallCLexicalAnalyzer {
+
+  /// <summary>
+  /// A <c>State</c> the <c>LexicalAnalyzer</c> can be in
+  /// Contains following methods:
+  /// <list>
+  /// <item>
+  /// <term>State</term>
+  /// <description>Initializes a new instance of the
+  /// <see cref="State{}"> class</description>
+  /// </item>
+  /// <item>
+  /// <term>State</term>
+  /// <description>Initializes a new instance of the
+  /// <see cref="State{string}"> class</description>
+  /// </item>
+  /// <item>
+  /// <term>AddToDictionary</term>
+  /// <description>Adds a key/value pair to the dictionary</description>
+  /// </item>
+  /// <item>
+  /// <term>GetDestination</term>
+  /// <description>Gets the destination state for a given <c>char</c>
+  /// </description>
+  /// </item>
+  /// </list>
+  /// </summary>
+  /// <remarks>
+  /// The <c>LexicalAnalyzer</c> can have one to many states
+  /// </remarks>
   class State {
+
+    /// <value>A private dictionary that represents the state transision for
+    /// for this <c>State</c> with keys of type <c>char</c> and values of type
+    /// <c>State</c></value>
     private Dictionary<char, State> transition = new Dictionary<char, State>();
 
+    /// <value>Gets and private sets the <c>TokenName</c></value>
     public string TokenName { get; private set; }
 
+    /// <summary>
+    /// Initialaizer for a  for a <c>State</c>
+    /// </summary>
+    /// <remarks>
+    /// Sets <paramref name="TokenName"/> to <c>null</c>
+    /// </remarks>
     public State() {
       TokenName = null;
     }
 
+    /// <summary>
+    /// Initializer for a <c>State</c>
+    /// </summary>
+    /// <remarks>
+    /// Sets <paramref name="TokenName"/> to <paramref name="tokenName"/>
+    /// </remarks>
+    /// <param name="tokenName">A <c>string</c></param>
     public State(string tokenName) {
       TokenName = tokenName;
     }
 
-    public bool AddDictionary(char c, State state) {
+    /// <summary>
+    /// Adds the value <paramref name="state"/> to the dictionary with key
+    /// <paramref name="c"/>
+    /// </summary>
+    /// <returns>
+    /// A <c>bool</c> if the add was successful or not
+    /// </returns>
+    /// <param name="c">A <c>char</c> as the key</param>
+    /// <param name="state">A <c>State</c> as the value</param>
+    public bool AddToDictionary(char c, State state) {
       try {
         transition.Add(c, state);
 
@@ -26,6 +82,13 @@ namespace SmallCLexicalAnalyzer {
       }
     }
 
+    /// <summary>
+    /// Gets the destination state given a <c>char</c> <paramref name="c"/>
+    /// </summary>
+    /// <returns>
+    /// A destination <c>State</c>
+    /// </returns>
+    /// <param name="c">A <c>char</c> as a key</param>
     public State GetDestination(char c) {
       return transition.TryGetValue(c, out State dest) ? dest : null;
     }
--- C#/LexicaAnalyzer/LexicaAnalyzer/Token.cs
+++ C#/LexicaAnalyzer/LexicaAnalyzer/Token.cs
@@ -2,6 +2,14 @@
 
   /// <summary>
   /// The <c>Token</c> struct
+  /// Contains following methods:
+  /// <list>
+  /// <item>
+  /// <term>Token</term>
+  /// <description>Initializes a new instance of the
+  /// <see cref="Token{string, string}"> class</description>
+  /// </item>
+  /// </list>
   /// </summary>
   /// <remarks>
   /// This is what the <c>LexicalAnalyer</c> returns when it is stepping
@@ -9,18 +17,18 @@
   /// </remarks>
   struct Token {
 
-    /// <value>Gets and sets the <c>Lexeme</c> property</value>
+    /// <value>Gets and private sets the <c>Lexeme</c> property</value>
     public string Lexeme { get; private set; }
 
-    /// <value>Gets and sets the <c>Name</c> property</value>
+    /// <value>Gets and private sets the <c>Name</c> property</value>
     public string Name { get; private set; }
 
     /// <summary>
-    /// Creates a <c>Token</c> using <paramref name="lexem"/> and
-    /// <paramref name="name"/>.
+    /// Initializer for a <c>Token</c> using <paramref name="lexem"/> and
+    /// <paramref name="name"/>
     /// </summary>
-    /// <param name="lexeme">A string.</param>
-    /// <param name="name">A string.</param>
+    /// <param name="lexeme">A string literal</param>
+    /// <param name="name">A string literal</param>
     public Token(string lexeme, string name) {
       Lexeme = lexeme;
       Name = name;
